d7efb41bfe5f130e83855b44c4a8a6b5a243d64c
Mathieu Dombrock <matdombrock@gmail.com>
2021-04-18 03:14:28 -0700

    Add .gitignore

--- output/core/libb64/cdecode.c.d
+++
@@ -1,7 +0,0 @@
-h:\LAB\Arduino\zIOTBP32\output\core\libb64\cdecode.c.o: \
- C:\Users\Mathieu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.5\cores\esp32\libb64\cdecode.c \
- C:\Users\Mathieu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.5\cores\esp32\libb64\cdecode.h \
- C:\Users\Mathieu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/stdint.h \
- C:\Users\Mathieu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/machine/_default_types.h \
- C:\Users\Mathieu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/sys/features.h \
- C:\Users\Mathieu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.5/tools/sdk/include/newlib/sys/_intsup.h
--- output/core/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-h:\LAB\Arduino\zIOTBP32\output\core\libb64\cencode.c.o: \
- C:\Users\Mathieu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.5\cores\esp32\libb64\cencode.c \
- C:\Users\Mathieu\AppData\Local\Arduino15\packages\esp32\hardware\esp32\1.0.5\cores\esp32\libb64\cencode.h
